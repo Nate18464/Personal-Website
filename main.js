9336d2c63cf54c773c45b6bb87879da6c069b375
Nathan <nrgwong@ucdavis.edu>
2021-09-06 16:58:48 -0700

    Completed Website

--- main.js
+++ main.js
@@ -5,11 +5,14 @@ let app = express();
 // ROUTING
 const viewRouter = require('./UI/routes/ViewRouter.js');
 
+// Asset Setup
+app.use('/Assets/NathanWong.jpg', express.static('Assets/NathanWong.jpg'));
+
 //Static file setup
 app.use(express.static(__dirname + '/UI/'));
 app.use('/', viewRouter.index);
 
 
 // hosting http server
-let port = process.env.PORT;
+let port = 5000;
 app.listen(port, console.log('NodeJS/Express server started on: ' + port));
